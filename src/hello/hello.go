26dacebc6078446c6ed3bacadf3db83974a07bd4
Practice Golang <practice.golang@gmail.com>
2018-08-09 03:46:54 +0900

    init

---
+++ src/hello/hello.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"mypkg"
+)
+
+func main() {
+	// Hello world
+	fmt.Println("Hello world!")
+	fmt.Println(mypkg.WorldName())
+
+	// Using package
+	var myName = "Chul-soo"
+	yourName := "Young-hee"
+
+	var name string
+	name = mypkg.MyName(myName)
+	fmt.Println(name)
+
+	fmt.Println(mypkg.YourName(yourName))
+}
---
+++ src/mypkg/getnames.go
@@ -0,0 +1,16 @@
+package mypkg
+
+// MyName : return my name
+func MyName(name string) string {
+	return "The name is " + name
+}
+
+// YourName : return your name
+func YourName(name string) string {
+	return "Your name is " + name
+}
+
+// WorldName : return hello world
+func WorldName() string {
+	return "안녕 세상!"
+}
